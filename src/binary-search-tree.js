4667d7f85dc57e17555b25d6c46fe539d82f386a
molletta <yasyule4ka@mail.ru>
2023-12-21 16:02:59 +0300

    feat: add solution for Queue task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,12 +1,13 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  
 
   root() {
     throw new NotImplementedError('Not implemented');
